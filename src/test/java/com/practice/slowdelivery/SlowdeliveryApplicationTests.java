d870efb899c8a804a4643a8601b63f9949f0f5d6
이연희 <ydol2@naver.com>
2022-07-28 23:12:02 +0900

    Initial commit

---
+++ src/test/java/com/practice/slowdelivery/SlowdeliveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practice.slowdelivery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SlowdeliveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
