192473089ac8bec327a95652ef14c8f29265b41c
이연희 <ydol2@naver.com>
2022-08-13 22:40:52 +0900

    [2022-08-13]

--- src/main/java/com/practice/slowdelivery/shop/application/ShopQueryService.java
+++ src/main/java/com/practice/slowdelivery/shop/application/ShopQueryService.java
@@ -1,4 +1,31 @@
 package com.practice.slowdelivery.shop.application;
 
+import com.practice.slowdelivery.exception.ErrorCode;
+import com.practice.slowdelivery.exception.NotFoundException;
+import com.practice.slowdelivery.shop.application.dto.ShopDetailInfo;
+import com.practice.slowdelivery.shop.application.dto.ShopSimpleInfo;
+import com.practice.slowdelivery.shop.infra.ShopQueryDao;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import static com.practice.slowdelivery.exception.ErrorCode.SHOP_NOT_FOUND;
+
+@RequiredArgsConstructor
+@Service
 public class ShopQueryService {
+
+    private final ShopQueryDao shopQueryDao;
+
+    @Transactional(readOnly = true)
+    public ShopSimpleInfo findSimpleInfo(long shopId){
+        return shopQueryDao.findSimpleInfo(shopId)
+                .orElseThrow(() -> new NotFoundException(SHOP_NOT_FOUND));
+    }
+
+    @Transactional(readOnly = true)
+    public ShopDetailInfo findDetailInfo(long shopId){
+        return shopQueryDao.findDetailInfo(shopId)
+                .orElseThrow(()-> new NotFoundException(SHOP_NOT_FOUND));
+    }
 }
