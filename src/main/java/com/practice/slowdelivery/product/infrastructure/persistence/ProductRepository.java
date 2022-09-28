a7c97a39fb90d270fa0a3b46a27eb0fe8e94def5
yeonx <71878202+yeonx@users.noreply.github.com>
2022-09-28 16:19:59 +0900

    Delete src/main/java/com/practice/slowdelivery/product directory

--- src/main/java/com/practice/slowdelivery/product/infrastructure/persistence/ProductRepository.java
+++
@@ -1,7 +0,0 @@
-package com.practice.slowdelivery.product.infrastructure.persistence;
-
-import com.practice.slowdelivery.product.domain.Product;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProductRepository extends JpaRepository<Product, Long> {
-}
