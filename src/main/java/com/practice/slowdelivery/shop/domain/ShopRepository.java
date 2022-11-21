beeefc8284204bff0eca1bc6b94ba1a25ead84bb
yeonx <71878202+yeonx@users.noreply.github.com>
2022-11-22 03:52:19 +0900

    Delete src/main/java/com/practice/slowdelivery/shop directory

--- src/main/java/com/practice/slowdelivery/shop/domain/ShopRepository.java
+++
@@ -1,9 +0,0 @@
-package be.shop.slow_delivery.shop.domain;
-
-
-import java.util.Optional;
-
-public interface ShopRepository {
-    Shop save(Shop shop);
-    Optional<Shop> findById(Long ShopId);
-}
--- src/main/java/com/practice/slowdelivery/shop/infra/ShopJpaRepository.java
+++
@@ -1,9 +0,0 @@
-package be.shop.slow_delivery.shop.infra;
-
-import be.shop.slow_delivery.shop.domain.Shop;
-import be.shop.slow_delivery.shop.domain.ShopRepository;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-
-public interface ShopJpaRepository extends JpaRepository<Shop, Long>, ShopRepository {
-}
