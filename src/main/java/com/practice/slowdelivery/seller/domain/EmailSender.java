f6075009667df2c79faba0840090c0ce9d6d2ee3
yeonx <71878202+yeonx@users.noreply.github.com>
2022-11-22 03:53:17 +0900

    Delete src/main/java/com/practice/slowdelivery/seller directory

--- src/main/java/com/practice/slowdelivery/seller/domain/EmailSender.java
+++
@@ -1,7 +0,0 @@
-package be.shop.slow_delivery.seller.domain;
-
-import be.shop.slow_delivery.seller.application.dto.EmailMessage;
-
-public interface EmailSender {
-    void send(EmailMessage emailMessage);
-}
