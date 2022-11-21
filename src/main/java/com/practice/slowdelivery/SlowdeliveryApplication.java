367cc1e1ea3cc934ac4151a8fc075a17d7592c7c
yeonx <71878202+yeonx@users.noreply.github.com>
2022-11-22 03:46:56 +0900

    Update SlowdeliveryApplication.java

--- src/main/java/com/practice/slowdelivery/SlowdeliveryApplication.java
+++ src/main/java/com/practice/slowdelivery/SlowdeliveryApplication.java
@@ -1,14 +1,15 @@
-package com.practice.slowdelivery;
+package be.shop.slow_delivery;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
+@EnableFeignClients
 @SpringBootApplication
-public class SlowdeliveryApplication {
+public class SlowDeliveryApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SlowdeliveryApplication.class, args);
+		SpringApplication.run(SlowDeliveryApplication.class, args);
 	}
 
 }
